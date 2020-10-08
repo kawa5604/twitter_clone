37cf5166de0ceaef06b3e69a041985810c359f14
Jorge G <jorge.gar@me.com>
2020-10-08 00:45:59 -0700

    Tie login screen to new LoginViewController.swift file

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Jorge Garcia on 10/8/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
