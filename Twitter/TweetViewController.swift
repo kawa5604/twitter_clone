06506a6e6c8189610f163635d6640c296e9b5c32
Jorge G <jorge.gar@me.com>
2020-10-18 20:53:09 -0700

    Add new nav controller and cancel/tweet buttons

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Jorge Garcia on 10/18/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
